198e7c2e13c2e2ff2e58dca132233291b37315b3
roy <roywashington1547@gmail.com>
2024-04-19 22:23:12 -0500

    schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,33 +4,130 @@
 import { sql } from "drizzle-orm";
 import {
   bigint,
-  index,
-  mysqlTableCreator,
+  boolean,
+  mysqlTable,
   timestamp,
   varchar,
+  int,
+  primaryKey,
+  mysqlEnum,
 } from "drizzle-orm/mysql-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = mysqlTableCreator(
-  (name) => `next-stripe-example_${name}`,
-);
+// notice how we are using a seperate table for users
+export const users = mysqlTable("users", {
+  id: varchar("id", { length: 255 }).notNull().primaryKey(),
+  name: varchar("name", { length: 256 }),
+  email: varchar("email", { length: 256 }),
+  password: varchar("password", { length: 256 }),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updatedAt").onUpdateNow(),
+});
 
-export const posts = createTable(
-  "post",
+export const oneTimeStripeCustomers = mysqlTable("one_time_stripe_customers", {
+  userId: varchar("user_id", { length: 255 })
+    .primaryKey()
+    .references(() => users.id),
+  stripeCustomerId: varchar("stripe_customer_id", { length: 256 }),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+
+  // these columns are how we will keep track of what the user has purchased
+  oneTimePaymentTutorialPurchased: boolean(
+    "one_time_payment_tutorial_purchased",
+  ).default(false),
+  subscriptionTutorialPurchased: boolean(
+    "subscription_tutorial_purchased",
+  ).default(false),
+  usageTutorialPurchased: boolean("usage_tutorial_purchased").default(false),
+});
+
+export const subcriptionStripeCustomers = mysqlTable(
+  "subscription_stripe_customers",
   {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    name: varchar("name", { length: 256 }),
+    userId: varchar("user_id", { length: 255 })
+      .primaryKey()
+      .references(() => users.id),
+    stripeCustomerId: varchar("stripe_customer_id", { length: 256 }),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updatedAt").onUpdateNow(),
+
+    // these columns are to keep track of the user's subscription tier and status
+    currentSubTier: mysqlEnum("current_sub_tier", [
+      "none",
+      "basic",
+      "premium",
+      "pro",
+    ]).default("none"),
+    currentSubStatus: mysqlEnum("current_sub_status", [
+      "active",
+      "inactive",
+      "canceled",
+    ]),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
+);
+
+// for usage we are going to do a credit based system
+// but it is also possible do something based around
+// the usage of a resource (compute, storage, etc)
+export const usageStripeCustomers = mysqlTable("usage_stripe_customers", {
+  userId: varchar("user_id", { length: 255 })
+    .primaryKey()
+    .references(() => users.id),
+  stripeCustomerId: varchar("stripe_customer_id", { length: 256 }),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+
+  credits: bigint("credits", { mode: "number" }).default(0),
+});
+
+// you can ingore these tables, they are for next-auth
+export const accounts = mysqlTable(
+  "account",
+  {
+    userId: varchar("userId", { length: 255 })
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    //  type: varchar("type", { length: 255 })
+    //   .$type<AdapterAccount["type"]>()
+    //   .notNull(),
+    provider: varchar("provider", { length: 255 }).notNull(),
+    providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
+    refresh_token: varchar("refresh_token", { length: 255 }),
+    access_token: varchar("access_token", { length: 255 }),
+    expires_at: int("expires_at"),
+    token_type: varchar("token_type", { length: 255 }),
+    scope: varchar("scope", { length: 255 }),
+    id_token: varchar("id_token", { length: 2048 }),
+    session_state: varchar("session_state", { length: 255 }),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  }),
+);
+
+export const sessions = mysqlTable("session", {
+  sessionToken: varchar("sessionToken", { length: 255 }).notNull().primaryKey(),
+  userId: varchar("userId", { length: 255 })
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+});
+
+export const verificationTokens = mysqlTable(
+  "verificationToken",
+  {
+    identifier: varchar("identifier", { length: 255 }).notNull(),
+    token: varchar("token", { length: 255 }).notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
   }),
 );
